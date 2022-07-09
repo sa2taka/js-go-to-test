c21b87f253f208a1c91591e0445adf3ba6003963
sa2taka <sa2taka@gmail.com>
2022-07-09 20:01:34 +0900

    :wrench: add jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  preset: 'ts-jest',
+  modulePaths: ['<rootDir>/'],
+  testPathIgnorePatterns: ['/__tests__/helpers/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.build.json',
+    },
+  },
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+};
